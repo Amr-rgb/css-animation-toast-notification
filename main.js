d96a459783b220b0bd750ece000d7fab5d6c1a94
Amr Taha <amrshiha2@gmail.com>
2022-07-03 05:25:42 +0200

    adding new feature functionality

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
 const toast = document.getElementById("toastWrapper");
+const firstText = document.getElementById("firstText");
+const lastText = document.getElementById("lastText");
 const btn = document.getElementById("btn");
 const closeBtn = document.getElementById("closeBtn");
 
-btn.addEventListener("click", () => {
-  toast.classList.add("active");
-  removeClass = setTimeout((_) => {
-    toast.classList.remove("active");
-  }, 5000);
-});
+const func = (status) => {
+  if (!toast.classList.contains("active")) {
+    toast.classList.remove("fail", "success");
+
+    if (status === "success") {
+      firstText.innerText = "Success";
+      lastText.innerText = "Your changes has been saved";
+    } else {
+      firstText.innerText = "Fail";
+      lastText.innerText = "Your changes has not been saved";
+    }
+
+    toast.classList.add(status);
+    toast.classList.add("active");
+    removeClass = setTimeout((_) => {
+      toast.classList.remove("active");
+    }, 5000);
+  }
+};
+
+btn.addEventListener("click", () =>
+  func(Math.round(Math.random()) ? "fail" : "success")
+);
 
 closeBtn.addEventListener("click", () => {
   toast.classList.remove("active");
   clearTimeout(removeClass);
 });
-
-console.log(closeBtn);
